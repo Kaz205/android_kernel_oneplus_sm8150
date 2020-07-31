95f53022310d9eaa626e731760ad33508b0f7abc
Kazuki Hashimoto <kaz205@tuta.io>
2020-08-01 00:25:42 +0900

    treewide: Revert cpufreq: Allow configuring default minimum frequencies in Kconfig
    CPU_INPUT_BOOST ends up overwriting those values. Revert these commits since a different approach is now used.
    
    This reverts commit b4e646b83491a890539031338b7d07c72a39588a.
    This reverts commit 7eacc549d1e44c41486a0bec0dcbb1c06dfe9695.
    This reverts commit 156291ce07b7e0eb0be1b018ddbf0ad5feb097f9
    
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -723,9 +723,6 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (&policy->object == &policy->min)				\
-		return count;						\
-									\
 	if (&policy->object == &policy->max)				\
 		return count;						\
 									\
